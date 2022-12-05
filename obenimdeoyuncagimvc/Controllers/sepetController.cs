6080a5585ebca69360ffccf18e9c87ac56d60f24
bilgecicek99 <83015725+bilgecicek99@users.noreply.github.com>
2022-12-05 14:10:32 +0300

    Update

--- obenimdeoyuncagimvc/Controllers/sepetController.cs
+++ obenimdeoyuncagimvc/Controllers/sepetController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using obenimdeoyuncagimvc.Models.Entity;
-
 
 namespace obenimdeoyuncagimvc.Controllers
 {
@@ -38,7 +38,7 @@ namespace obenimdeoyuncagimvc.Controllers
 
             db.sepet.Add(x);
             db.SaveChanges();
-            return View("Index");
+            return RedirectToAction("Index");
         }
         [HttpPost]
         public ActionResult temizle(string barkodno)
@@ -48,6 +48,50 @@ namespace obenimdeoyuncagimvc.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        [HttpPost]
+        public ActionResult uyebilgiekle(FormCollection form)
+        {
+            System.Diagnostics.Debug.WriteLine(form["barkodno"]+"1215156156");
+            var DbInstanceuye = db.uye.Find(form["tc"]);
+           
+            var DbIntancesepet = db.sepet.Find(form["barkodno"]);
+            uye x = new uye();
+            sepet y = new sepet();  
+            x.tc = DbInstanceuye.tc;
+            x.adsoyad = DbInstanceuye.adsoyad;
+            x.yas= DbInstanceuye.cinsiyet;
+            x.cinsiyet = DbInstanceuye.cinsiyet;
+            x.telefon = DbInstanceuye.telefon;
+            y.barkodno = form["barkodno"];
+            y.oyuncakadi = DbIntancesepet.oyuncakadi;
 
+            y.ureticifirma = DbIntancesepet.ureticifirma;
+            y.oyuncakboyutu = DbIntancesepet.oyuncakboyutu;
+            y.oyuncakyasgrubu = DbIntancesepet.oyuncakyasgrubu;
+            y.oyuncaksayisi = DbIntancesepet.oyuncaksayisi;
+            y.teslimtarihi = "28.11.2022";
+            y.iadetarihi = "28.11.2022";
+            emanetoyuncaklar eo = new emanetoyuncaklar();
+            eo.tc = x.tc;
+            eo.adsoyad = x.adsoyad;
+            eo.yas = x.yas;
+            eo.telefon = x.telefon;
+            eo.barkodno = form["barkodno"];
+            eo.oyuncakadi = y.oyuncakadi;
+            eo.ureticifirma = y.ureticifirma;
+            eo.oyuncakboyutu = y.oyuncakboyutu;
+            eo.oyuncakyasgrubu = y.oyuncakyasgrubu;
+            eo.oyuncaksayisi = y.oyuncaksayisi;
+            eo.teslimtarihi = y.teslimtarihi;
+            eo.iadetarihi = y.iadetarihi;
+
+            db.emanetoyuncaklar.Add(eo);
+           
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
     }
 }
